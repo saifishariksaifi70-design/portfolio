7ff4e05fb3178932ebaeb70ada9d423e9a9d4d45
Sharik saifi <saifishariksaifi70@gmail.com>
2026-07-16 11:12:25 +0530

    Add

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useState } from 'react'
 import { Link } from 'react-router-dom';
+import { Menu, X } from "lucide-react";
 
 
 const Navbar = (props) => {
@@ -13,7 +14,7 @@ const Navbar = (props) => {
                 <img src="https://img.icons8.com/?size=96&id=nqg2tDAxO1LG&format=gif&color=f7f7f7" alt=""  className='w-[50px] h-[50px]'/>
                 </h1>
             <div className=''>
-                <ul className='flex gap-10 text-gray-300'>
+                <ul className='flex gap-10 text-gray-300 md:hidden flex gap-8 text-lg'>
                     <li className='hover:text-sky-500 cursor-pointer'>
                         <Link to="/">Home</Link>
                     </li>
@@ -27,7 +28,29 @@ const Navbar = (props) => {
                         <Link to="/Contact">Contact</Link>
                     </li>
                 </ul>
+                <button className='md:hidden'
+                onClick={()=>setMenuOpen(!menuOpen)}
+                >
+                    {menuOpen ? <X size={30}/> : <Menu size={30} /> }
+
+                </button>
             </div>
+            {menuOpen && (
+                <ul className='flex gap-10 text-gray-300 md:hidden flex gap-8 text-lg'>
+                    <li className='hover:text-sky-500 cursor-pointer'>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li className='hover:text-sky-500 cursor-pointer'>
+                        <Link to="/About">About</Link>
+                    </li>
+                    <li className='hover:text-sky-500 cursor-pointer'>
+                        <Link to="/Skills">Skills</Link>
+                    </li>
+                    <li className='hover:text-sky-500 cursor-pointer'>
+                        <Link to="/Contact">Contact</Link>
+                    </li>
+                </ul>
+            )}
             <button className='bg-sky-500 w-[150px] h-[50px] rounded-2xl hover:bg-sky-700'>
                 <h1 className='text-[15px]'><a href="C:\Users\Admin\Downloads\Sharik_Saifi_Resume.pdf">Download Resume</a></h1>
             </button>
