4404adf9e7fb4d5b78379697b5972d1983f99c28
Sharik saifi <saifishariksaifi70@gmail.com>
2026-08-03 11:24:05 +0530

    s

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='flex justify-center items-center pb-10 pt-5 bg-slate-900'>
+    <div className='flex justify-center items-center pb-10 pt-5 bg-slate-900 sm:max-h-full md:max-h-full'>
         <div className='items-center'>
-        <h1 className='text-white text-2xl'>@ 2026  
+        <h1 className='text-white text-2xl text-center'>@ 2026  
             <span className='bg-gradient-to-r from-sky-500 to-purple-300 bg-clip-text text-transparent font-semibold'> Sharik Saifi.</span>
             All right Reserved</h1>
             <p className='text-center text-white/60'>Design & Developer</p>
